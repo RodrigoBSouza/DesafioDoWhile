9d6b63f4373406b9a263710b6e0679fe7bea6d45
Rodrigo Souza <rodrigbsouza@gmail.com>
2021-12-10 11:44:05 -0300

    DesafioDoWhile

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function click1() {
+    document.querySelector('#box2').innerHTML = '<p>Hoje, principalmente neste momento de pandemia, mais do que nunca, ficou provado que a tecnologia veio para ficar na educação. Não se pode imaginar um cenário onde exista educação sem a tecnologia. Mas a realidade em nosso país infelizmente não é essa. Uma pesquisa recente do Datafolha mostra que 51% dos alunos da rede pública ainda não têm acesso a computador com internet. Em um cenário como o nosso, onde os alunos ficaram em média 279 dias sem aula, boa parte destes alunos não puderam acompanhar as aulas online justamente pelo fato de não serem alcançados pela tecnologia no lugar onde moram.<p>';
+    document.getElementById('click2').style.backgroundColor = '#FFF';
+    document.getElementById('click1').style.backgroundColor = '#DDD';
+}
+
+function click2() {
+    document.querySelector('#box2').innerHTML = '<p>Por mais que ainda tenhamos dificuldades relacionadas à infraestrutura, conectividade e formação, os professores acreditam no potencial da tecnologia. Mais da metade destes professores recomendam o uso de tecnologia nos centros educacionais. Porém, não adianta colocar computadores e diversos recursos técnológicos se os alunos não tem uma base para usufruir deles o que há de melhor. </p>'
+    document.getElementById('click1').style.backgroundColor = '#FFF';
+    document.getElementById('click2').style.backgroundColor = '#DDD';
+    document.getElementById('#box2').style.height = '371px';
+}
+
+function click3() {
+    document.querySelector('#box3').innerHTML = '<p>Sem dúvidas estamos vivendo uma revolução tecnológica que passa pela transformação cultural e econômica das empresas e da sociedade. A começar pelos smartphones, computadores, televisores de última geração, a tecnologia tem alcançado nossos hospitais, meios de transportes, meios de comunicação, mercado de trabalho, e chegando ao tão esperado sinal 5G que possibilitará mais avanços tecnológicos em nosso país com a previsão de que todas as cidades do país tenham internet até 2028, incluindo comunidades rurais.<p>';
+    document.getElementById('click4').style.backgroundColor = '#FFF';
+    document.getElementById('click3').style.backgroundColor = '#DDD';
+}
+
+function click4() {
+    document.querySelector('#box3').innerHTML = '<p>A prova de que o crescimento tecnológico tem sido grande em nosso país é a procura por profissionais da área de TI por exemplo, que segundo a plataforma de empregos Revelo, aumentou 480% entre janeiro e outubro comparado com o mesmo período em 2020. Enquanto algumas profissões estão desaparecendo, empresas nacionais e internacionais têm disputado profissionais da área de TI. Uma pesquisa recente prevê carência de 408 mil profissionais de TI até 2022 no Brasil. Atualmente nosso país é o 10º maior mercado do mundo no setor, na América Latina é o líder e responde por 40% do total segundo a Associação Brasileira das Empresas de Software (Abes).</p>';
+    document.getElementById('click3').style.backgroundColor = '#FFF';
+    document.getElementById('click4').style.backgroundColor = '#DDD';
+}
+
+function click5() {
+    document.querySelector('#box6').innerHTML = '<p>Apenas a tecnologia não basta para ajudar na redução da desiguldade social que enfrentamos em nosso país. Com toda certeza a solução deste problema não está em apenas ter acesso à tecnologia. Seria relativamente fácil para o governo investir na disponibilização de internet e equipamentos para as pessoas menos favorecidas.<p>';
+    document.getElementById('click6').style.backgroundColor = '#FFF';
+    document.getElementById('click5').style.backgroundColor = '#DDD';
+}
+
+function click6() {
+    document.querySelector('#box6').innerHTML = '<p>Penso que, além de investimento em tecnologia, também se faz necessário investir nestas pessoas com cursos, faculdades, treinamentos, alcançando e capacitando quem quer que seja a não somente ter acesso ao que a tecnologia pode proporcionar mas também preparando estas pessoas para construir um futuro melhor. A tecnologia tem o poder de transformar a realidade das pessoas, é através dela que iremos construir um futuro mais desenvolvido, com mais igualdade entre as pessoas.</p>';
+    document.getElementById('click5').style.backgroundColor = '#FFF';
+    document.getElementById('click6').style.backgroundColor = '#DDD';
+}
+
+function scroll1() {
+    window.scrollTo(0, 50);
+}
+
+function scroll2() {
+    window.scrollTo(0, 650);
+}
+
+function scroll3() {
+    window.scrollTo(0, 1200);
+}
